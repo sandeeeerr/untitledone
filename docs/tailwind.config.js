ca4a42f911dd98c05f90860c3063833dcb4eed15
sandeeeerr <sandervries@me.com>
2025-09-04 21:58:55 +0200

    Docs/site (#24)
    * docs(site): add Nextra docs app
    
    * Edit tsconfig.json to exclude the 'docs' folder from TypeScript compilation.
    
    * Voeg 'type' toe aan package.json voor module-ondersteuning in de documentatie.
    
    * Update Nextra configuratie en afhankelijkheden in package.json
    
    - Verander de import van Nextra naar de juiste module.
    - Update versies van Next.js, React, React-DOM, Nextra en Nextra-theme-docs naar specifieke versies voor betere stabiliteit.
    
    * Voeg Node.js versie 20.x toe aan de engines in package.json voor compatibiliteit.
    
    * Update documentatieconfiguratie en verwijder verouderde bestanden
    
    - Wijzig de Nextra-configuratie in next.config.mjs om zoekfunctionaliteit en standaard kopieeropties in te schakelen.
    - Pas package.json aan voor de nieuwe naam en beschrijving van het project, en voeg een postbuild-script toe voor pagina-indexering.
    - Verwijder onnodige bestanden zoals theme.config.tsx, _app.tsx, en verschillende pagina's in de documentatie om de structuur te vereenvoudigen.
    - Update tsconfig.json om de TypeScript-configuratie te verbeteren en de compatibiliteit te waarborgen.
    
    * Voeg Tailwind CSS, Autoprefixer en PostCSS toe aan package.json voor verbeterde styling en compatibiliteit.
    
    * Verwijder verouderde bestanden en update configuratie
    
    - Verwijder LICENSE, README.md, en verschillende afbeeldingen uit de documentatie.
    - Pas .gitignore aan om extra bestanden zoals .vercel en .DS_Store uit te sluiten.
    - Update next.config.mjs voor Nextra-configuratie.
    - Wijzig package.json om de naam en beschrijving van het project te reflecteren, en voeg nieuwe afhankelijkheden toe voor verbeterde styling met Tailwind CSS.
    
    * Update documentatieconfiguratie en verbeter TypeScript-instellingen
    
    - Voeg ESLint- en TypeScript-instellingen toe aan next.config.mjs om bouwfouten te negeren.
    - Wijzig module-resolutie in tsconfig.json naar 'bundler' voor betere compatibiliteit.
    - Pas layout.tsx aan voor verbeterde metadata en banner-instellingen.
    - Verwijder verouderde pagina's in de documentatie om de structuur te vereenvoudigen.

---
+++ docs/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './content/**/*.{js,ts,jsx,tsx,mdx}',
+    './mdx-components.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
